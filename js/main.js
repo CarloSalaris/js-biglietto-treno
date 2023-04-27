42532371f2a787a9988c0b6e3763b1db5cc8e7e6
CarloSalaris <carloa.salaris@gmail.com>
2023-04-27 16:06:34 +0200

    HTML + JS comments

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+// chiedere all’utente (prompt();) :
+
+    // il numero di chilometri che vuole percorrere [userKm] (parseInt();)
+
+    // l’età del passeggero [userAge] (parseInt();)
+
+// calcolare il prezzo totale del viaggio, secondo queste regole:
+
+    // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+    // Sconto (?):
+        // IF userAge < 18 {sconto del 20%}
+        // ELSE IF userAge >= 65 {sconto del 40%}
+        // ELSE no discount
+
+// Output prezzo finale con massimo due decimali (.toFixed())
