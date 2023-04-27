f37d17d32619c7bf4aa82582dd7b8b22ca3b5dfc
CarloSalaris <carloa.salaris@gmail.com>
2023-04-27 17:12:34 +0200

    Centralised discount + "let" to "const"

--- js/main.js
+++ js/main.js
@@ -1,40 +1,34 @@
-// Global variables
-let userKm, userAge, ticketPrice;
-
 
 // chiedere all’utente (prompt();) :
 
     // il numero di chilometri che vuole percorrere [userKm] (parseInt();)
-    userKm = parseInt(prompt("Quanti chilometri devi percorrere?"));
+    const userKm = parseInt(prompt("Quanti chilometri devi percorrere?"));
 
     // l’età del passeggero [userAge] (parseInt();)
-    userAge = parseInt(prompt("Quanti anni hai?"));
+    const userAge = parseInt(prompt("Quanti anni hai?"));
 
 // calcolare il prezzo totale del viaggio, secondo queste regole:
 
     // il prezzo del biglietto è definito in base ai km (0.21 € al km)
-    ticketPrice = userKm * 0.21;
-
-    console.log("prezzo biglietto senza sconto", ticketPrice);
+    const ticketPrice = userKm * 0.21;
 
     // Sconto (?):
+
+    let discount = (ticketPrice / 100);
         
         if (userAge < 18) { // IF userAge < 18 {sconto del 20%}
 
-            ticketPrice = ticketPrice - (ticketPrice / 100) * 20;
+            discount = discount * 20;
 
         }else if (userAge >= 65) { // ELSE IF userAge >= 65 {sconto del 40%}
             
-            ticketPrice = ticketPrice - (ticketPrice / 100) * 40;
+            discount = discount * 40;
 
-        } else {// ELSE no discount
-            
-            ticketPrice = ticketPrice;
         }
-
-        console.log("prezzo biglietto finale", ticketPrice);
-        
+        else {
+            discount = 0;
+        }        
 
 // Output prezzo finale con massimo due decimali (.toFixed())
 
-        document.getElementById("output_id").innerHTML = `L'importo totale del biglietto sarà di ${ticketPrice.toFixed(2)} euro`;
+        document.getElementById("output_id").innerHTML = `L'importo totale del biglietto sarà di ${(ticketPrice - discount).toFixed(2)} euro`;
